4d52d6c35a325ebfe5114bb87723a31e3c5c2114
Caio Raposo <caioraposo@protonmail.com>
2020-10-04 13:14:29 -0300

    Read the template file and execute dijsktra

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use graph_rs::graph::Graph;
 
 fn main() {
-    let mut graph = Graph::new(1,1);
-    let v = vec!["test", "again"];
-    graph.add_from_str_vec(v);
-    graph.add_undirected_edge(0, 1, 10);
+    let mut graph = Graph::new(13,16);
 
-    assert_eq!(graph.get_node_index("test"), Some(0));
-    assert_eq!(graph.get_node_index("again"), Some(1));
+    let input: Vec<&str> = include_str!("../template").lines().collect();
+    let mut input = input.iter();
+    // Add nodes
+    graph.add_from_str_vec(input.next().unwrap().split_whitespace().collect());
+
+    let heuristics: Vec<u64> = input.next()
+                                    .unwrap()
+                                    .split_whitespace()
+                                    .map(|x| x.parse().unwrap())
+                                    .collect();
+    // Add edges
+    for line in input {
+        if line.len() == 0 { break; } // EOF
+        let e: Vec<&str> = line.split_whitespace().collect();
+        graph.add_undirected_edge_str(e[0], e[1], e[2].parse().unwrap());
+    }
+
+    println!("{}", graph);
+    println!("Dijsktra");
+    let (dist, path) = graph.dijsktra(0, 12, false);
+    println!("{:?} {:?}", dist, path);
+
+    println!();
+    println!("{}", graph);
 }
